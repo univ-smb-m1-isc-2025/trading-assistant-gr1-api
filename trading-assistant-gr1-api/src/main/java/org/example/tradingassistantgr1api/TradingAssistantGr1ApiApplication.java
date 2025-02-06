33c9a9bd017733d184462583eb46e88a515241aa
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2025-02-06 10:49:31 +0100

    init spring boot project

---
+++ trading-assistant-gr1-api/src/main/java/org/example/tradingassistantgr1api/TradingAssistantGr1ApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.tradingassistantgr1api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingAssistantGr1ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingAssistantGr1ApiApplication.class, args);
+    }
+
+}
---
+++ trading-assistant-gr1-api/src/test/java/org/example/tradingassistantgr1api/TradingAssistantGr1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tradingassistantgr1api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingAssistantGr1ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
