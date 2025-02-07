b2861454f514bf74b204adacebd48b0a76ede7bc
Cyril Pitar <cyrilpitar@orange.fr>
2025-02-07 18:19:10 +0100

    Un petit pas pour l'API

---
+++ src/main/java/me/trading_assistant/api/controller/controller.java
@@ -0,0 +1,29 @@
+package me.trading_assistant.api.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/info")
+    public Map<String, String> info() {
+        return Map.of(
+            "name", "Trading Assistant API",
+            "version", "1.0.0"
+        );
+    }
+    
+}
