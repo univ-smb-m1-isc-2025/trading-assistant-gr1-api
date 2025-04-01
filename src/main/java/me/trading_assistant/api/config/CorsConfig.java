3fa72dc3a5b427ca755a71403f86e82568f572e6
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2025-04-01 11:09:47 +0200

    🚧 Fix CorsConfig

--- src/main/java/me/trading_assistant/api/config/CorsConfig.java
+++ src/main/java/me/trading_assistant/api/config/CorsConfig.java
@@ -2,23 +2,42 @@ package me.trading_assistant.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Autorise toutes les routes
-                        .allowedOrigins("http://localhost:5173", "https://www.trademate.oups.net") // Autorise toutes les origines (remplacez par des domaines spécifiques si nécessaire)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Autorise les méthodes HTTP
-                        .allowedHeaders("*") // Autorise tous les en-têtes
-                        .allowCredentials(true); // Autorise l'envoi de cookies
-            }
-        };
+    public CorsFilter corsFilter() {
+        // Créer une configuration CORS
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("http://localhost:5173");
+        corsConfig.addAllowedOrigin("http://www.trademate.oups.net");
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("PUT");
+        corsConfig.addAllowedMethod("DELETE");
+        corsConfig.addAllowedMethod("OPTIONS");
+        corsConfig.addAllowedHeader("*"); // Autoriser tous les en-têtes
+        corsConfig.setAllowCredentials(true); // Si vous devez autoriser les cookies ou l'authentification
+
+        // Appliquer la configuration à toutes les URLs
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsFilter(source); // Créer et retourner un CorsFilter
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://www.trademate.oups.net")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
