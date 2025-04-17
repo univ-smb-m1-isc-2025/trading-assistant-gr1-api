b27a5b75dad9431525adc53d5bba3af471dd545f
Cyril Pitar <cyrilpitar@orange.fr>
2025-04-17 12:18:25 +0200

    Fix Cors

--- src/main/java/me/trading_assistant/api/config/CorsConfig.java
+++ src/main/java/me/trading_assistant/api/config/CorsConfig.java
@@ -1,42 +1,24 @@
 package me.trading_assistant.api.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
+public class CorsConfig {
     @Bean
-    public CorsFilter corsFilter() {
-        // Créer une configuration CORS
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOrigin("http://localhost:5173");
-        corsConfig.addAllowedOrigin("https://www.trademate.oups.net");
-        corsConfig.addAllowedMethod("GET");
-        corsConfig.addAllowedMethod("POST");
-        corsConfig.addAllowedMethod("PUT");
-        corsConfig.addAllowedMethod("DELETE");
-        corsConfig.addAllowedMethod("OPTIONS");
-        corsConfig.addAllowedHeader("*"); // Autoriser tous les en-têtes
-        corsConfig.setAllowCredentials(true); // Si vous devez autoriser les cookies ou l'authentification
-
-        // Appliquer la configuration à toutes les URLs
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        System.out.println("CORS configuration applied: " + corsConfig);
-
-        return new CorsFilter(source); // Créer et retourner un CorsFilter
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173", "https://www.trademate.oups.net/","http://localhost:80") // URL de ton front
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
-
-
-// curl -X POST http://localhost:8080/api/users \
-// -H "Origin: http://localhost:5173" \
-// -H "Content-Type: application/json" \
-// -d '{"firstname": "Mathis","lastname": "Feltrin","email": "mathis.feltrin@gmail.com","password": "meilleur test","phone": "0133555566"}'
